a7143d7bf018a39f5d90b1627c009c9eb8316ba9
CEKIRO <152188406+CEKIRO@users.noreply.github.com>
2023-11-29 22:44:49 +0100

    Update streamlit_app.py
    syntax correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_fruit_load_list():
     return my_curr.fetchall()
 
 #Add a button to load the fruit
-if streamli.button('Get fruit load list'):
+if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
